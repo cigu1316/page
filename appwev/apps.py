281a35a5f3c2e28d7c931f0ed49876512a1baee9
gustavo <cigu1966@gamil.com>
2023-02-15 16:43:32 -0300

    appwev

---
+++ appwev/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppwevConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'appwev'
---
+++ appwev/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render 
+
+
+  
+def home(request):
+    
+    return render(request,"appwev/home.html")
+
+def servicios(request):
+    
+    return render(request,'appwev/servicios.html')
+
+def tienda(request):
+    
+    return render(request,'appwev/tienda.html')
+
+def blog(request):
+    
+    return render(request,'appwev/blog.html')
+
+def contacto(request):
+    
+    return render (request,'appwev/contacto.html')
